f74e62bd9e1db4b0d3a182182801162fab459ecb
ahmedinfa <info.infa02@gmail.com>
2026-01-20 00:56:36 +0600

    complete cash out functionalities

--- main.js
+++ main.js
@@ -23,3 +23,26 @@ addMoneyBtn.addEventListener("click", function (e) {
     alert("please enter valid input");
   }
 });
+
+const cashOutBtn = document.getElementById("cashOutBtn");
+cashOutBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  const cashOutForm = document.getElementById("cashOutForm");
+  const agentNumber = document.getElementById("agentNumber");
+  const cashOutAmount = document.getElementById("cashOutAmount");
+  const cashOutPin = document.getElementById("cashOutPin");
+  const mainBalance = document.getElementById("mainBalance");
+  const convertedMainBalance = parseInt(mainBalance.innerText);
+  const convertedCashOutAmount = parseInt(cashOutAmount.value);
+
+  if (
+    agentNumber.value.length === 11 &&
+    convertedCashOutAmount &&
+    cashOutPin.value === "4700"
+  ) {
+    mainBalance.innerText = convertedMainBalance - convertedCashOutAmount;
+    cashOutForm.reset();
+  } else {
+    alert("please enter valid input");
+  }
+});
