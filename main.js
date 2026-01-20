c89e6d11c48952fc2f9dfaff6a52582b160d74f2
ahmedinfa <info.infa02@gmail.com>
2026-01-20 15:19:31 +0600

    complete transfer money functionalities

--- main.js
+++ main.js
@@ -24,6 +24,7 @@ addMoneyBtn.addEventListener("click", function (e) {
   }
 });
 
+// cash out functionalities
 const cashOutBtn = document.getElementById("cashOutBtn");
 cashOutBtn.addEventListener("click", function (e) {
   e.preventDefault();
@@ -42,6 +43,32 @@ cashOutBtn.addEventListener("click", function (e) {
   ) {
     mainBalance.innerText = convertedMainBalance - convertedCashOutAmount;
     cashOutForm.reset();
+  } else {
+    alert("please enter valid input");
+  }
+});
+
+// transfer money functionalities
+
+const transferBtn = document.getElementById("transferBtn");
+transferBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const transferMoneyForm = document.getElementById("transferMoneyForm");
+  const userAccountNumber = document.getElementById("userAccountNumber");
+  const transferAmount = document.getElementById("transferAmount");
+  const transferPin = document.getElementById("transferPin");
+  const mainBalance = document.getElementById("mainBalance");
+  const convertedMainBalance = parseInt(mainBalance.innerText);
+  const convertedTransferAmount = parseInt(transferAmount.value);
+
+  if (
+    userAccountNumber.value.length === 11 &&
+    transferAmount.value &&
+    transferPin.value === "4700"
+  ) {
+    mainBalance.innerText = convertedMainBalance - convertedTransferAmount;
+    transferMoneyForm.reset();
   } else {
     alert("please enter valid input");
   }
